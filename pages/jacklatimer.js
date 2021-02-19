5dc9c663e0c1e02ded4aba09f6a7f80792f10464
Jack Latimer <jacklatimer03@gmail.com>
2021-02-19 14:17:23 +0000

    Description for jacklatimer.dev

--- pages/jacklatimer.js
+++ pages/jacklatimer.js
@@ -33,6 +33,9 @@ export default function project() {
           jacklatimer.dev is my very own personal website build with Next.js and Tailwind CSS hosted on ▲Vercel.
         </h2>
         <img src="./images/jacklatimer.webp"></img>
+        <h2 className="mb-10 text-gray-600 dark:text-gray-400">
+          Coming Soon.
+        </h2>
         <h2 className="mt-5 mb-10 space-x-4 text-gray-900 dark:text-gray-400">
           
         </h2>
