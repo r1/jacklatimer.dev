9a0ecabea39c8a0335fa050c1481749bb5efafaa
Jack Latimer <jacklatimer03@gmail.com>
2021-02-22 15:57:24 +0000

    GA

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,8 +1,8 @@
 import ReactGA from 'react-ga'
-
+require('dotenv').config();
 export const initGA = () => {
 // https://gyazo.com/3101aeec0a80c2e637e8856449ddf071 Select these settings when you are setting up your analytics to get a tracking ID
-  ReactGA.initialize("UA-152031643-2")
+  ReactGA.initialize(process.env.GA_KEY)
 }
 
 export const logPageView = () => {
