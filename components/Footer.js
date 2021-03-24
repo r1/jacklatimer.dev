6a78edd936643b68ec65080ac9db4e26271ab22c
Jack Latimer <jacklatimer03@gmail.com>
2021-03-24 11:25:18 +0000

    done(footer): changed text colour

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const ExternalLink = ({ href, children }) => (
   <a
-    className="text-black hover:text-gray-900 dark:text-white dark:hover:text-gray-300 transition"
+    className="text-black hover:text-gray-900 dark:text-gray-600 dark:hover:text-gray-700 transition"
     target="_blank"
     rel="noopener noreferrer"
     href={href}
