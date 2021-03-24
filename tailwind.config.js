f7cae7702dc71db58271de39f97dbb031cb669e0
Jack Latimer <jacklatimer03@gmail.com>
2021-03-24 10:22:22 +0000

    do(tailwind config): change colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        gray: {
+          100: "#FBFBFB",
+          200: "#EAEAEA",
+          300: "#DFDFDF",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#333333",
+          900: "#191919",
+        },
+      },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans]
       },
@@ -24,45 +37,6 @@ module.exports = {
             'blockquote p:last-of-type::after': false
           }
         },
-        dark: {
-          css: {
-            color: theme('colors.gray.300'),
-            a: {
-              color: theme('colors.blue.400'),
-              '&:hover': {
-                color: theme('colors.blue.600')
-              },
-              code: { color: theme('colors.blue.400') }
-            },
-            blockquote: {
-              borderLeftColor: theme('colors.gray.700'),
-              color: theme('colors.gray.300')
-            },
-            h2: { color: theme('colors.gray.100') },
-            h3: { color: theme('colors.gray.100') },
-            h4: { color: theme('colors.gray.100') },
-            hr: { borderColor: theme('colors.gray.700') },
-            ol: {
-              li: {
-                '&:before': { color: theme('colors.gray.500') }
-              }
-            },
-            ul: {
-              li: {
-                '&:before': { backgroundColor: theme('colors.gray.500') }
-              }
-            },
-            strong: { color: theme('colors.gray.300') },
-            thead: {
-              color: theme('colors.gray.100')
-            },
-            tbody: {
-              tr: {
-                borderBottomColor: theme('colors.gray.700')
-              }
-            }
-          }
-        }
       })
     }
   },
