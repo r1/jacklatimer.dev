ecdbdbc2cbc5eb7c6a2c4539454b939e6e5753ea
Jack Latimer <jacklatimer03@gmail.com>
2021-05-25 04:15:32 +0100

    done(colors): removed light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}',],
-  darkMode: 'class',
   theme: {
     extend: {
       colors: {
